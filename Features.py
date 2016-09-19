fa8dd8dfd0574a0fd4310f49a7cbae5d31cd6615
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-19 18:48:28 +0530

    Skeleton for Features object

---
+++ Features.py
@@ -0,0 +1,14 @@
+class Features:
+    def __init__(self):
+        self.google_snippets_match = 0
+        self.word_count = 0
+        self.long_word = 0
+        self.noun_count = 0
+        self.adj_count = 0
+        self.adv_count = 0
+        self.spelling_errors = 0
+        self.essay_length = 0
+        self.sentence_count = 0
+
+    def initialize_pos(self):
+        pass
