5d3535dd81d00d51efd2232f57ffcca050589c47
Pranav Dhakras <dhakrasp@yahoo.co.in>
2016-10-01 15:38:31 +0530

    Adding basic RandomForest implementation

--- Kappa.py
+++ Kappa.py
@@ -6,6 +6,9 @@ Created on Wed Sep 21 18:55:38 2016
 """
 
 import skll.metrics as metric
+import numpy as np
+import math
+import statistics as stats
 
 # Calculates the average quadratic kappa for the entire essay set
 def get_average_kappa(targets, predictions):
@@ -17,3 +20,37 @@ def get_average_kappa(targets, predictions):
 
 	avg_kappa  = float(total_kappa) / float(num)
 	return avg_kappa
+ 
+def avg_minimum_squared_error(targets, predictions):
+    length = len(targets)
+    assert(length == len(predictions))
+    assert(type(targets)==np.ndarray and type(predictions) == np.ndarray)
+    
+    diff = 1.0*(targets - predictions)
+    mse = sum(diff**2)/2
+    return mse/length
+
+def avg_chi_square(targets, predictions):
+    length = len(targets)
+    assert(length == len(predictions))
+    assert(type(targets)==np.ndarray and type(predictions) == np.ndarray)
+    
+    diff = 1.0*(targets - predictions)
+    chi_square = sum(diff**2 / np.sqrt(targets))
+    return chi_square/length
+    
+# Data is a vector (1xn or nx1)
+def get_variance(data):
+    assert(type(data) == np.ndarray)
+    mean = stats.mean(data)
+    length = 1.0*len(data)
+    diff = data - mean
+    return sum(diff**2)/length
+        
+def get_weighted_variance(variance_1, length_1, variance_2, length_2):
+    total_length = 1.0*(length_1 + length_2)
+    prob_1 = length_1/total_length
+    prob_2 = length_2/total_length
+    weighted_var = variance_1*prob_1 + variance_2*prob_2
+    return weighted_var
+     
--- Node.py
+++ Node.py
@@ -6,9 +6,31 @@ Created on Thu Sep 29 22:07:18 2016
 """
 
 class Node:
-    def __init__(self, column, value, left_child, right_child):
+    def __init__(self, column = None, split_value = None, left_child = None, right_child = None, result = None):
         self.column = column
-        self.value = value
-        self. left_child = left_child
+        self.split_value = split_value
+        self.left_child = left_child
         self.right_child = right_child
-        self.result = -1
+        self.result = result
+    
+    def display(self, level = 0):
+        indent = level*5
+        if self.result is None:
+            print("-"*indent + str((self.column, self.split_value)) + '?')
+        else:
+            print("-"*indent + 'Predict : ' + str(self.result))
+        if self.left_child is not None:
+            self.left_child.display(level+1)
+        if self.right_child is not None:
+            self.right_child.display(level+1)
+            
+    def predict(self, sample):
+        prediction = -1
+        if self.result is not None:
+            prediction = self.result
+        else:
+            if sample[self.column] < self.split_value:
+                prediction = (self.left_child).predict(sample)
+            else:
+                prediction = (self.right_child).predict(sample)
+        return prediction
